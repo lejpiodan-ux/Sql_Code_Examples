87fde7d91eabf871037c3c6cfa804c27bf53e2b6
lejpiodan-ux <lejpiodan@gmail.com>
2025-12-14 22:16:29 +0100

    Add files via upload

---
+++ wINDOW FUNCKEJFEIFIE.sql
@@ -0,0 +1,18 @@
+
+select
+	o.OrderID,
+	O.CustomerID,
+	round(SUM(ord.UnitPrice*ord.Quantity*(1-ord.Discount)),0) as [ Total Order Sum ],
+	coalesce(
+		round(
+			abs(
+			SUM(ord.UnitPrice*ord.Quantity*(1-ord.Discount))
+		   -LAG(SUM(ord.UnitPrice*ord.Quantity*(1-ord.Discount)))
+			over (partition by o.customerid  order by o.orderid))
+		,0)
+	,0)as [Current - Previous]
+
+from dbo.[Order Details] ord
+inner join dbo.Orders o 
+	on o.OrderID = ord.OrderID
+group by o.orderid,o.customerid,o.OrderDate
